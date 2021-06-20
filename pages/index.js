670dc23709afa8fa5bd1108cb2c374d5688bec64
mawavial <panzerkovic@gmail.com>
2021-06-20 17:21:13 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ pages/index.js
@@ -0,0 +1,55 @@
+import Head from "next/head";
+import Link from "next/link";
+import Date from "../components/date";
+import Layout, { siteTitle } from "../components/layout";
+import { getSortedPostsData } from "../lib/posts";
+import utilStyles from '../styles/utils.module.css'
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+        <section className={utilStyles.headingMd}>
+        <p>
+          Hey! I'm Mateus. I'm a software engineer. I've been developing
+          software since I was a kid. I started out programming RPG games using
+          RPGMaker. Later I started learning html and css. When javascript was
+          not yet a thing...Yeap, that long ago And now I'm like... Oh my god...
+          Is this for real? Server side rendering. Where should things go. Where
+          are my console.logs...
+        </p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{" "}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
